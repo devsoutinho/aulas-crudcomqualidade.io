f1d72541c2e9e35fb6d958c089a54d55c633fe2b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-03-21 22:36:36 -0300

    update

--- core/crud.ts
+++ core/crud.ts
@@ -4,12 +4,48 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [
+    ...read(),
+    todo,
+  ];
+
   // salvar o content no sistema
-  fs.writeFileSync(DB_FILE_PATH, content);
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+    dogs: [],
+  }, null, 2));
   return content;
 }
 
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if(!db.todos) { // Fail Fast Validations
+    return [];
+  }
+
+  return db.todos;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
 
 // [SIMULATION]
-console.log(create("...asdusdahhdusauhas"));
+CLEAR_DB()
+create("Primeira TODO");
+create("Segunda TODO");
+console.log(read());
