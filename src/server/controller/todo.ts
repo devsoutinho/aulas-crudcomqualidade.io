f9f18da3aa7494e51849862433f824edae79ce78
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-04-09 20:49:49 -0300

    A5.1

--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -1,7 +1,8 @@
-import { todoRepository } from "@server/repository/todo";
 import { NextApiRequest, NextApiResponse } from "next";
+import { z as schema } from "zod";
+import { todoRepository } from "@server/repository/todo";
 
-function get(req: NextApiRequest, res: NextApiResponse) {
+async function get(req: NextApiRequest, res: NextApiResponse) {
   const query = req.query;
   const page = Number(query.page);
   const limit = Number(query.limit);
@@ -35,6 +36,31 @@ function get(req: NextApiRequest, res: NextApiResponse) {
   });
 }
 
+const TodoCreateBodySchema = schema.object({
+  content: schema.string(),
+});
+async function create(req: NextApiRequest, res: NextApiResponse) {
+  // Fail Fast Validations
+  const body = TodoCreateBodySchema.safeParse(req.body);
+  // Type Narrowing
+  if (!body.success) {
+    res.status(400).json({
+      error: {
+        message: "You need to provide a content to create a TODO",
+        description: body.error.issues,
+      },
+    });
+    return;
+  }
+  // Here we have the data!
+  const createdTodo = await todoRepository.createByContent(body.data.content);
+
+  res.status(201).json({
+    todo: createdTodo,
+  });
+}
+
 export const todoController = {
   get,
+  create,
 };
