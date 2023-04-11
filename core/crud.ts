1dcede859178d0d590138196fcac5ecb513980c2
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-04-11 00:42:42 -0300

    update

--- core/crud.ts
+++ core/crud.ts
@@ -1,12 +1,15 @@
 import fs from "fs"; // ES6
 import { v4 as uuid } from 'uuid';
-// const fs = require("fs"); - CommonJS
+// const fs = require("fs"); - CommonJS 
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+
+type UUID = string;
+
 interface Todo {
-  id: string;
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
@@ -43,7 +46,7 @@ function read(): Array<Todo> {
   return db.todos;
 }
 
-function update(id: string, partialTodo: Partial<Todo>): Todo {
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
   let updatedTodo;
   const todos = read();
   todos.forEach((currentTodo) => {
@@ -64,16 +67,25 @@ function update(id: string, partialTodo: Partial<Todo>): Todo {
   return updatedTodo;
 }
 
-function updateContentById(id: string, content: string): Todo {
+function updateContentById(id: UUID, content: string): Todo {
   return update(id, {
     content,
   })
 }
 
-function deleteById(id: string) {
+function deleteById(id: UUID) {
   const todos = read();
 
-  
+  const todosWithoutOne = todos.filter((todo) => {
+    if(id === todo.id) {
+      return false;
+    }
+    return true;
+  });
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos: todosWithoutOne,
+  }, null, 2));
 }
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, "");
@@ -82,9 +94,9 @@ function CLEAR_DB() {
 // [SIMULATION]
 CLEAR_DB();
 create("Primeira TODO");
-const secondTodo = create("Primeira TODO");
+const secondTodo = create("Segunda TODO");
 deleteById(secondTodo.id);
-const thirdTodo = create("Segunda TODO");
+const thirdTodo = create("Terceira TODO");
 // update(thirdTodo.id, {
 //   content: "Atualizada!",
 //   done: true,
