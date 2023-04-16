36f91ae03cb6af77ddf2835abb4cf83062790d2a
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-04-15 22:27:37 -0300

    A6.3 Parte 02

---
+++ src/server/schema/todo.ts
@@ -0,0 +1,19 @@
+import { z as schema } from "zod";
+
+export const TodoSchema = schema.object({
+  id: schema.string().uuid(),
+  content: schema.string().nonempty(),
+  // .datetime()
+  date: schema.string().transform((date) => {
+    return new Date(date).toISOString();
+  }),
+  // .boolean()
+  done: schema.string().transform((done) => {
+    if(done === "true") {
+      return true;
+    }
+    return false;
+  }),
+});
+
+export type Todo = schema.infer<typeof TodoSchema>;
